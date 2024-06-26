ba83cad2f34b8a636dfaff0a7cc0c987a39b6c55
RAJAS BHINGARDE <rajasbhingarde18@gmail.com>
2024-06-26 22:34:06 +0530

    initial commits

--- inference.py
+++ inference.py
@@ -1,7 +1,7 @@
 import joblib
 from preprocessing import preprocess_text
 
-# Load model and vectorizer
+
 model = joblib.load('model/fake_news_model.pkl')
 vectorizer = joblib.load('model/vectorizer.pkl')
 
